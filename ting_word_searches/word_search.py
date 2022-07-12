6794d2e4b69f06eb6c58c234080a76692ae1e6de
GustavoAns <gugasato9@live.com>
2022-07-12 18:31:07 -0300

    Add req 6 e 7 Arquivo: word_search.py

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,29 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+import re
 
+def exists_word(word, instance):
+    max_texts = len(instance)
+    all_finds = []
+    for i in range(max_texts):
+        obj = instance.search(i)
+        arquivo_caminho = obj['nome_do_arquivo']
+        ocorrencias = []
+        for raw_index in range(len(obj['linhas_do_arquivo'])):
+            if re.search(word, obj['linhas_do_arquivo'][raw_index], re.IGNORECASE):
+                ocorrencias.append({'linha': raw_index + 1})
+        if len(ocorrencias) > 0:
+            all_finds.append({'palavra': word, 'arquivo': arquivo_caminho, 'ocorrencias': ocorrencias})
+    return all_finds
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    max_texts = len(instance)
+    all_finds = []
+    for i in range(max_texts):
+        obj = instance.search(i)
+        arquivo_caminho = obj['nome_do_arquivo']
+        ocorrencias = []
+        for raw_index in range(len(obj['linhas_do_arquivo'])):
+            if re.search(word, obj['linhas_do_arquivo'][raw_index], re.IGNORECASE):
+                ocorrencias.append({'linha': raw_index + 1, 'conteudo': obj['linhas_do_arquivo'][raw_index]})
+        if len(ocorrencias) > 0:
+            all_finds.append({'palavra': word, 'arquivo': arquivo_caminho, 'ocorrencias': ocorrencias})
+    return all_finds
