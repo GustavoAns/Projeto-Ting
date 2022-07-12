7027c8d614155a2f960123eecbc51e8af87a0403
GustavoAns <gugasato9@live.com>
2022-07-12 16:42:09 -0300

    Add req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,11 @@
+import sys
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    formato = path_file.split('.')[-1]
+    if formato != 'txt':
+        sys.stderr.write('Formato inválido\n')
+    try:
+        with open(path_file, 'r', encoding='utf-8') as arquivo:
+            return arquivo.read().split('\n')
+    except FileNotFoundError:
+        sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
